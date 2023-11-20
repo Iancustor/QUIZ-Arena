b178cf06e7dd39fb096506c10d9452139d4d772e
Ian Sebugenyi <140151921+Iancustor@users.noreply.github.com>
2023-11-20 15:23:54 +0300

    update

---
+++ typed.js
@@ -0,0 +1,10 @@
+import Typed from "typed.js";
+const typed = new Typed(".welcome", {
+  strings: [
+    "<i>Your Welcome To The Quiz Arena ,</i>  Insert in your Name and Password .",
+    ". . . Enjoy the Ultimate power of The Brain ! ",
+    ". . .  sign In to continue to the Next Page !!",
+    "!! THE QUIZ ARENA !!",
+  ],
+  typeSpeed: 55,
+});
